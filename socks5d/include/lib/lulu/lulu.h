cde301c9d1582cfb132bafbe42ed9891a1f5ca22
luciatorrusio <lucia.torrusio@gmail.com>
2022-06-14 20:33:45 -0300

    hello client parser without tests

---
+++ socks5d/include/lib/lulu/lulu.h
@@ -0,0 +1,8 @@
+//
+// Created by tluci on 11/6/2022.
+//
+
+#ifndef SOCKS5D_LULU_H
+#define SOCKS5D_LULU_H
+
+#endif //SOCKS5D_LULU_H
---
+++ socks5d/lib/lulu/lulu.c
@@ -0,0 +1,5 @@
+//
+// Created by tluci on 11/6/2022.
+//
+
+#include "lulu/lulu.h"
