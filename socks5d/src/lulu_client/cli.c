7b88740dd0f9ff860f269b56ede1667b88d95d31
luciatorrusio <lucia.torrusio@gmail.com>
2022-06-21 11:15:40 -0300

    print version, print help

--- socks5d/src/lulu_client/cli.c
+++ socks5d/src/lulu_client/cli.c
@@ -5,6 +5,7 @@
 #include <getopt.h>
 #include <string.h>
 #include <errno.h>
+#include <stdio.h>
 #include "lulu_client/cli.h"
 #include "utils/utils.h"
 #include "utils/logger.h"
@@ -71,9 +72,15 @@ CliArguments ParseCli(int argc, char ** argv){
 }
 
 void PrintVersion() {
-
+    printf("Version 0.0.1\n");
 }
 
 void PrintHelp() {
-
+    printf("LULU\n");
+    printf("OPCIONES\n");
+    printf("\t-h     Imprime la ayuda y termina.\n\n");
+    printf("\t-L dirección-de-management\n\t\tEstablece la dirección donde servirá el servicio de management.\n\t\tPor defecto escucha únicamente en loopback.\n\n");
+    printf("\t-P puerto-conf\n\t\tPuerto SCTP  donde escuchará por conexiones entrante del protocolo\n\t\tde configuración. Por defecto el valor es 8080.\n\n");
+    printf("\t-u user:pass\n\t\tDeclara un usuario del server LULU con su contraseña.\n\n");
+    printf("\t-v     Imprime información sobre la versión versión y termina.\n\n");
 }
