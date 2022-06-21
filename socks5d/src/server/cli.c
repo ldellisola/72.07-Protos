6f65a048d5c3f76a287511e8c14c0246435057b1
luciatorrusio <lucia.torrusio@gmail.com>
2022-06-21 11:36:09 -0300

    validate parameters

--- socks5d/src/server/cli.c
+++ socks5d/src/server/cli.c
@@ -7,6 +7,7 @@
 #include <strings.h>
 #include <getopt.h>
 #include <errno.h>
+#include <arpa/inet.h>
 #include "server/cli.h"
 #include "utils/utils.h"
 #include "utils/logger.h"
@@ -14,6 +15,7 @@
 
 void PrintVersion();
 void PrintHelp();
+bool isValidIpAddress(char *ipAddress);
 
 void LoadUser(CliArguments * args,int userNum, char * user);
 
@@ -34,22 +36,38 @@ CliArguments ParseCli(int argc, char **argv) {
     int numberOfUsers = 0;
     int ch;
     long value;
+    int p;
     while ((ch = getopt(argc,argv,"hl:L:Np:P:u:vb:t:")) != -1){
         switch (ch) {
             case 'l':
-                args.SocksAddress = optarg;
+                if (isValidIpAddress(optarg))
+                    Fatal("Invalid socks Address");
+                else
+                    args.SocksAddress = optarg;
                 break;
             case 'N':
                 args.EnablePasswordScanners = false;
                 break;
             case 'L':
-                args.LuluAddress = optarg;
+                if (isValidIpAddress(optarg))
+                    Fatal("Invalid Lulu Adress");
+                else
+                    args.LuluAddress = optarg;
                 break;
             case 'p':
-                args.SocksPort = optarg;
+                p = atoi(optarg);
+                if(p >0 && p <= 65535)
+                    args.SocksPort = optarg;
+                else
+                    Fatal("Invalid socksPort");
                 break;
             case 'P':
-                args.LuluPort = optarg;
+                p = atoi(optarg);
+                if(p >0 && p <= 65535)
+                    args.LuluPort = optarg;
+                else
+                    Fatal("Invalid LuluPort");
+
                 break;
             case 'b':
                 value = strtol(optarg, null, 10);
@@ -116,5 +134,14 @@ void PrintHelp() {
     printf("\t-P puerto-conf\n\t\tPuerto SCTP  donde escuchará por conexiones entrante del protocolo\n\t\tde configuración. Por defecto el valor es 8080.\n\n");
     printf("\t-u user:pass\n\t\tDeclara un usuario del proxy con su contraseña. Se puede utilizar\n\t\thasta 10 veces.\n\n");
     printf("\t-v     Imprime información sobre la versión versión y termina.\n\n");
+}
+
+bool isValidIpAddress(char *ipAddress)
+{
+    struct sockaddr_in sa;
+    int result = inet_pton(AF_INET, ipAddress, &(sa.sin_addr));
+    if(result == 0)
+        result = inet_pton(AF_INET6, ipAddress, &(sa.sin_addr));
+    return result != 0;
 }
 
